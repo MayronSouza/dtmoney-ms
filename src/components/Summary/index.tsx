261153230996f51a141cd0601cc2ff4808a06548
MayronSouza <mayron.souza@gmz.solutions>
2021-09-06 13:20:44 -0300

    finalizando a estrutura do componente Summary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,35 @@
+import incomeImg from '../../assets/images/income.svg';
+import outcomeImg from '../../assets/images/outcome.svg';
+import totalImg from '../../assets/images/total.svg';
+
 import { Container } from './styles';
 
 export function Summary() {
   return (
     <Container>
-      <h1>Summary</h1>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+        <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+
+      <div className="high-lighting">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Saídas" />
+        </header>
+        <strong>R$0,00</strong>
+      </div>
     </Container>
   );
 }
